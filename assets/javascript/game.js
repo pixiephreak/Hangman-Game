fde0b8fa599d5fdfaa5c72b64203d65f65343fe0
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-02-25 17:32:18 -0500

    update get request

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -169,7 +169,7 @@ controller.init();
 // $.get('http://www.setgetgo.com/randomword/get.php', {len:5}, (data) => controller.updateData(data));
 // $.get('http://www.setgetgo.com/randomword/get.php', (data) => controller.updateData(data));
 
-$.get('https://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5' , (data) => controller.updateData(data));
+$.get('http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5' , (data) => controller.updateData(data));
 
   //get synonyms and give hints
 
